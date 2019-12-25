bbb27cc387cdad908b4a16ecc17199fd066fa5ef
Subhi Pandey <subhi2012cpi@gmail.com>
2019-12-26 01:28:53 +0530

    First Commit

---
+++ lib/camerascreen/camera_screen.dart
@@ -0,0 +1,90 @@
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() {
+    return _CameraScreenState();
+  }
+}
+
+class _CameraScreenState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Click To Share'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: _cameraPreviewWidget(),
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _cameraTogglesRowWidget(),
+                  _captureControlRowWidget(context),
+                  Spacer()
+                ],
+              ),
+              SizedBox(height: 20.0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  
+  Widget _cameraPreviewWidget() {
+    
+    return Container();
+  }
+
+  
+  Widget _captureControlRowWidget(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FloatingActionButton(
+                child: Icon(Icons.camera),
+                backgroundColor: Colors.blueGrey,
+                onPressed: () {
+                  _onCapturePressed(context);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  
+  Widget _cameraTogglesRowWidget() {
+    
+    return Spacer();
+  }
+
+  void _onCapturePressed(context) async {
+    
+  }
+
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
+    print('Error: ${e.code}\n${e.description}');
+  }
+}
